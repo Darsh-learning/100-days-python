ada9faa61957db59bd9346065cd96b5a348e1728
Darsh-learning <darshf036@gmail.com>
2025-06-05 13:39:48 +0530

    Day 3 Completedgit add .

--- Day-3/TreasureFinder.py
+++ Day-3/TreasureFinder.py
@@ -19,7 +19,26 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 *******************************************************************************'''
 
-print(ascii_art)
 
 print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
 print("You are at a crossroad")
+if input('Where would you like to go?\n"left" or "right"\n').lower() == "left":
+    print('You are now at the Ganga river. Would you swim across or wait for a boat?\n"swim" or "wait"')
+    if input().lower() == "wait":
+        print("Now there are three doors in front of you.")
+        print("Red, Blue and Yellow. Treasure is behind one of the door.")
+        print('Which door would you enter? "Red", "Blue" or "Yellow"')
+        doorImp = input().lower()
+        if doorImp == "yellow":
+            print("Congratulations!! You won the game.")
+            print(ascii_art)
+        elif doorImp == "red":
+            print("You got burned by fire.\nGame Over")
+        elif doorImp == "blue":
+            print("You got eaten by beasts.\n Game Over.")
+        else:
+            print("Invalid Input.\nPlease Try Again.")
+    else:
+        print("You got attacked by a shark.\nGame Over.")
+else:
+    print("You fell into a hole.\nGame Over.")
